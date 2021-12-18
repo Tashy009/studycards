e2edcb62b62476a631ddf6e6684782773575d0bb
Tashy009 <shittutaofeek009@gmail.com>
2021-12-18 06:47:47 +0100

    client build

--- app.js
+++ app.js
@@ -42,7 +42,9 @@ app.use("/api/v1/flashcard", authenticateUser, flashCardRouter);
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(
+    path.resolve(__dirname, "../client/studycards/build", "index.html")
+  );
 });
 
 app.use(notFoundMiddleware);
